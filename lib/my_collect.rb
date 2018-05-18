5e12979fb2255ee2480b4f75970de608ca79c313
GregBenn <gbenn07@gmail.com>
2018-05-18 08:16:36 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
+def my_collect(empty_array)
+  counter = 0
+  filled_array = []
+  while counter < empty_array.length
+    filled_array <<
+yield(empty_array[counter])
+    counter += 1
+  end
+  filled_array
+end
 
 
